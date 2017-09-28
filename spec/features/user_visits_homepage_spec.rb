8a3407b7df33254aae91ca7676858d918eb9dc9f
pleblan2 <pleblan2@asu.edu>
2017-09-27 19:05:41 -0700

    Added homepage smoketest

---
+++ spec/features/user_visits_homepage_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+feature "User visits homepage" do
+
+	#Makes sure the user has visited the homepage
+	scenario "successfully" do
+		#Goes to the root path for testing
+		visit root_path
+
+		#The browser will visit the root path and see if css tag h1 has Todos in it
+		#page is provided by capybara
+		expect(page).to have_css 'h1', text: 'Todos'
+	end
+end
