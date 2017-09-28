83adc24b839c332f07cc5420ca9fa30a37e0f3b2
pleblan2 <pleblan2@asu.edu>
2017-09-28 10:40:30 -0700

    Allow a user to create a todo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'todos/index'
+
+  resources :todos, only: [:index, :new, :create]
+	root to: "todos#index"
 end
