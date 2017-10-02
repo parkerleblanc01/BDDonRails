6e9c59f1217a646aaecfb9847a361cef2f7bf572
pleblan2 <parkerjleblanc@gmail.com>
2017-10-02 16:09:14 -0700

    Created user object and referenced todos and email from that object instead of string

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,7 @@
 class TodosController < ApplicationController
   before_filter :authenticate
   def index
-  	@todos = Todo.where(email: current_email)
+  	@todos = current_user.todos
   end
 
   def new
@@ -9,7 +9,8 @@ class TodosController < ApplicationController
   end
 
   def create
-  	Todo.create(todo_params.merge(email: current_email))
+    current_user.todos.create(todo_params)
+  	Todo.create(todo_params.merge(email: @email))
   	redirect_to todos_path
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002201619) do
+ActiveRecord::Schema.define(version: 20171002224224) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "email"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
